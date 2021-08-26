75c10ae50e96ae68098bfbcb63cb1c96a5da85c0
hornhuang <814484626@qq.com>
2021-08-26 11:09:42 +0800

    fix：单元测试文件路径
    fix：单元测试文件路径

--- test/tabcontrollerweb_test.dart
+++ test/tabcontrollerweb_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:tabcontrollerweb/web_tab_controller.dart';
+import 'package:tab_controller_web/web_tab_controller.dart';
 
 void main() {
   test('adds one to input values', () {
